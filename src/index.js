8d80859a99c32dd065974665d71bf876ea26077e
mkhomenko2212 <mkhomenko2212@gmail.com>
2022-05-10 14:20:08 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import './css/styles.css';
-import Notiflix from 'notiflix'
-import { fetchCountries } from './js/fetch-countries'
-import debounce from 'lodash.debounce'
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { fetchCountries } from './js/fetchCountries';
+import countryCard from './templates/country-card.hbs';
+import countryList from './templates/country-list.hbs';
 
 const DEBOUNCE_DELAY = 300;
+const debounce = require('lodash.debounce');
+
 
 const inputEl = document.querySelector('#search-box');
 const countriesListEl = document.querySelector('.country-list');
