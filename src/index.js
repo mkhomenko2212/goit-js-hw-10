2be557953d32f2b9de2b6cee644700337718fe10
mkhomenko2212 <mkhomenko2212@gmail.com>
2022-05-11 22:55:30 +0200

    add

--- src/index.js
+++ src/index.js
@@ -1,47 +1,71 @@
+import './css/styles.css'
+import debounce from 'lodash.debounce'
+import Notiflix from 'notiflix'
+import { fetchCountries } from './js/fetch-countries'
 
+const DEBOUNCE_DELAY = 300
 
-import './css/styles.css';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import { fetchCountries } from './js/fetch-countries';
-import countryCard from './templates/country-card.hbs';
-import countryList from './templates/country-list.hbs';
+const countryInput = document.querySelector('#search-box')
+const countryList = document.querySelector('.country-list')
+const countryInfo = document.querySelector('.country-info')
 
-const DEBOUNCE_DELAY = 300;
-const debounce = require('lodash.debounce');
+countryInput.addEventListener('input', debounce(onCountryInput, DEBOUNCE_DELAY))
 
+function onCountryInput() {
+  const name = countryInput.value.trim()
+  if (name === '') {
+    return (countryList.innerHTML = ''), (countryInfo.innerHTML = '')
+  }
 
-const inputEl = document.querySelector('#search-box');
-const countriesListEl = document.querySelector('.country-list');
-const cardContainerEl = document.querySelector('.country-info');
+  fetchCountries(name)
+    .then(countries => {
+      countryList.innerHTML = ''
+      countryInfo.innerHTML = ''
+      if (countries.length === 1) {
+        countryList.insertAdjacentHTML('beforeend', renderCountryList(countries))
+        countryInfo.insertAdjacentHTML('beforeend', renderCountryInfo(countries))
+      } else if (countries.length >= 10) {
+        alertTooManyMatches()
+      } else {
+        countryList.insertAdjacentHTML('beforeend', renderCountryList(countries))
+      }
+    })
+    .catch(alertWrongName)
+}
 
+function renderCountryList(countries) {
+  const markup = countries
+    .map(({ name, flags }) => {
+      return `
+          <li class="country-list__item">
+              <img class="country-list__flag" src="${flags.svg}" alt="Flag of ${name.official}" width = 30px height = 30px>
+              <h2 class="country-list__name">${name.official}</h2>
+          </li>
+          `
+    })
+    .join('')
+  return markup
+}
 
+function renderCountryInfo(countries) {
+  const markup = countries
+    .map(({ capital, population, languages }) => {
+      return `
+        <ul class="country-info__list">
+            <li class="country-info__item"><p><b>Capital: </b>${capital}</p></li>
+            <li class="country-info__item"><p><b>Population: </b>${population}</p></li>
+            <li class="country-info__item"><p><b>Languages: </b>${Object.values(languages).join(', ')}</p></li>
+        </ul>
+        `
+    })
+    .join('')
+  return markup
+}
 
-const onCountriesFetch = (event) => {
-    const inputValue = event.target.value.trim();
-    if (inputValue.length === 0) {
-        countriesListEl.innerHTML = "";
-        return;
-    } else {
-        fetchCountries(inputValue)
-        .then(onMarkupCountryCard)
-            .catch( () => {
-                countriesListEl.innerHTML = "";
-                Notify.failure('Oops, there is no country with that name');
-            });
-    };
-} 
+function alertWrongName() {
+  Notiflix.Notify.failure('Oops, there is no country with that name')
+}
 
-inputEl.addEventListener('input', debounce(onCountriesFetch, DEBOUNCE_DELAY));
-
-
-// Добавляем разметку
-const onMarkupCountryCard = (countries) => {
-    if (countries.length > 10) {
-        cardContainerEl.innerHTML = "";
-        return Notify.info('Too many matches found. Please enter a more specific name.');
-    } if (countries.length === 1) {
-        cardContainerEl.innerHTML = countryCard(countries[0]);
-    } else {
-        countriesListEl.innerHTML = countryList(countries);
-    }
+function alertTooManyMatches() {
+  Notiflix.Notify.info('Too many matches found. Please enter a more specific name.')
 }
--- src/js/fetch-countries.js
+++ src/js/fetch-countries.js
@@ -1,12 +1,9 @@
 
+const BASE_URL = 'https://restcountries.com/v3.1/name/'
+const fields = 'fields=name,capital,population,flags,languages'
+
 export function fetchCountries(name) {
-    return fetch(
-        `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
-        .then(response => {
-            if (!response.ok) {
-                throw Error(response.statusText);
-            }
-            return response.json();
-        })
-    .catch(error => console.err(error));
-};
+  return fetch(`${BASE_URL}${name}?${fields}`)
+    .then(response => response.json())
+    .catch(error => console.log(error))
+}
